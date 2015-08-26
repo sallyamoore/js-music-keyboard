2e6f34b048cc9c9f49db3611b2992422da9c6ce0
Sally Moore <sallyamoore@gmail.com>
2015-08-25 17:11:41 -0700

    Functionality to play note on click

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
 $(document).ready( function() {
-  // your code here
+
+  function playOnClick(note) {
+    $("." + note).click(function() {
+      playAudio(note);
+    });
+  }
+
+  function playAudio(note) {
+    var audio_tag = document.getElementById(note + 'Audio');
+    audio_tag.currentTime = 0;
+    audio_tag.play();
+  }
+
+  playOnClick("c");
+  playOnClick("d");
+  playOnClick("e");
+  playOnClick("f");
+  playOnClick("g");
+  playOnClick("a");
+  playOnClick("b");
 });
